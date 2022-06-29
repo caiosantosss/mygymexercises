e4db60c49fe5e2c0036708a5816a30ac0815a6d9
caiosantosss <caio.santos@msn.com>
2022-06-29 20:32:09 +0900

    add the exercide video to the exercise detail

--- src/components/ExerciseVideos.js
+++ src/components/ExerciseVideos.js
@@ -1,10 +1,36 @@
 import React from 'react';
-import { Box, Slack, Typography } from '@mui/material';
+import { Box, Stack, Typography } from '@mui/material';
 
 
 const ExerciseVideos = ({ exerciseVideos, name }) => {
+
+  console.log(exerciseVideos);
+
+  if (!exerciseVideos.length) return 'Loading...';
   return (
-    <div>ExerciseVideos</div>
+    <Box sx={{ marginTop: { lg: '200px', xs: '20px' } }} p="20px">
+      <Typography variant="h4" mb="33px">
+        Watch <span style={{ color: '#ff2625', textTransform: 'capitalize' }}>{name}</span> exercises videos
+      </Typography>
+      <Stack justifyContent="flex-start" flexWrap="wrap" alignItems="center"
+        sx={{
+          flexDirection: { lg: 'row' },
+          gap: { lg: '120px', xs: '0' },
+        }}
+      >
+        {exerciseVideos?.slice(0, 3).map((item, index) => (
+          <a
+            key={index}
+            className="exercise-video-link"
+            href={`https://www.youtube.com/watch?v=${item.video.channelId}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={item.video.thumbnails[0].url} alt={item.video.title} />
+          </a>
+        ))}
+      </Stack>
+    </Box>
   )
 }
 
