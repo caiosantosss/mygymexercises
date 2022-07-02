e25c14bc9ade2a1ba3d2b2a21957371e828e0833
caiosantosss <caio.santos@msn.com>
2022-07-02 23:06:54 +0900

    add Loader

---
+++ src/components/Loader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Stack } from '@mui/material'
+import { infinitySpin } from 'react-loader-spinner'
+
+const Loader = () => {
+  return (
+    <div>Loader</div>
+  )
+}
+
+export default Loader
