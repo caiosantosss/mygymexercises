bebe1ef21a26904b9d57150d5bc7242bcb22faff
caiosantosss <caio.santos@msn.com>
2022-07-05 22:28:10 +0900

    add style to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,12 @@ import Logo from '../assets/images/Logo-1.png';
 const Footer = () => {
   return (
     <Box mt="80px" bgcolor="#fff3f4">
-
+      <Stack gap="40px" alignItems="center" px="40px" pt="24px">
+        <img src={Logo} alt="logo" width="200px" height="40px" />
+        <Typography variant="h5">
+          Made with ❤️ by Caio Santos. © 2022
+        </Typography>
+      </Stack>
     </Box>
   )
 }
